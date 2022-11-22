0e8368f3695b7e94722c64b67656c6c05cba0370
JayantTeotia16 <jayantteotia19@gmail.com>
2022-11-23 00:41:55 +0530

    Bottom nav testing

--- app/src/main/java/com/example/gulliver_v2/MainActivity.kt
+++ app/src/main/java/com/example/gulliver_v2/MainActivity.kt
@@ -1,33 +1,34 @@
 package com.example.gulliver_v2
 
 import android.content.Context
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.AttributeSet
 import android.view.MenuItem
 import android.view.View
+import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
-import androidx.constraintlayout.widget.ConstraintSet
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
+import androidx.fragment.app.Fragment
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
-
-import androidx.navigation.ui.NavigationUI.setupActionBarWithNavController
-import androidx.navigation.ui.NavigationUI.setupWithNavController
 import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.example.gulliver_v2.databinding.ActivityMainBinding
 import com.example.tiptime.data.Datasource
-import com.google.android.material.appbar.CollapsingToolbarLayout
+import com.google.android.material.bottomappbar.BottomAppBar
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationBarView
 import com.google.android.material.navigation.NavigationView
 
-class MainActivity : AppCompatActivity(R.layout.activity_main), NavigationView.OnNavigationItemSelectedListener {
+
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener,  NavigationBarView.OnItemSelectedListener {
+    private lateinit var binding: ActivityMainBinding
     private lateinit var navController: NavController
     lateinit var toolbar: Toolbar
     private lateinit var appBarConfiguration: AppBarConfiguration
@@ -36,6 +37,8 @@ class MainActivity : AppCompatActivity(R.layout.activity_main), NavigationView.O
     lateinit var navView: NavigationView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         val navHostFragment = supportFragmentManager
             .findFragmentById(R.id.gulli_nav_frag) as NavHostFragment
@@ -52,26 +55,20 @@ class MainActivity : AppCompatActivity(R.layout.activity_main), NavigationView.O
         toolbar.setupWithNavController( navController, appBarConfiguration)
 
         navView.setNavigationItemSelectedListener(this)
-
-        BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        binding.bottomNavigation.setOnItemSelectedListener { item ->
             when(item.itemId) {
-                R.id.home_page -> {
-                    Toast.makeText(this, "Profile clicked", Toast.LENGTH_SHORT).show()
-                    true
-                }
-                R.id.cart_page -> {
-                    // Respond to navigation item 2 click
-                    true
-                }
+                R.id.home_page -> replaceFragment(HomeFragment())
+                R.id.cart_page -> replaceFragment(CartFragment())
+                R.id.explore_page -> replaceFragment(GMapsFragment())
+                R.id.profile_page -> replaceFragment(ProfileFragment())
                 else -> false
             }
+            true
         }
 
-        }
-
-    override fun onCreateView(name: String, context: Context, attrs: AttributeSet): View? {
-        return super.onCreateView(name, context, attrs)
     }
+
+
 
     private fun onListItemClick(position: Int) {
         val myDataset = Datasource().loadAffirmations()
@@ -86,13 +83,11 @@ class MainActivity : AppCompatActivity(R.layout.activity_main), NavigationView.O
         val navController = findNavController(R.id.gulli_nav_frag)
         when (item.itemId) {
             R.id.nav_profile -> {
-                Toast.makeText(this, "Profile clicked", Toast.LENGTH_SHORT).show()
-                navController.navigate(R.id.action_homeFragment_to_GMapsFragment)
+                replaceFragment(ProfileFragment())
             }
             R.id.nav_messages -> {
-                Toast.makeText(this, "Messages clicked", Toast.LENGTH_SHORT).show()
-                navController.navigate(R.id.action_GMapsFragment_to_itineraryFragment)
-            }
+
+                            }
             R.id.nav_friends -> {
                 Toast.makeText(this, "Friends clicked", Toast.LENGTH_SHORT).show()
             }
@@ -107,10 +102,16 @@ class MainActivity : AppCompatActivity(R.layout.activity_main), NavigationView.O
         return true
     }
 
-    private fun setupBottomNavMenu(navController: NavController) {
-        val bottomNav = findViewById<BottomNavigationView>(R.id.bottom_navigation)
-        bottomNav?.setupWithNavController(navController)
+    private fun replaceFragment(fragment : Fragment){
+
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.gulli_nav_frag,fragment)
+        fragmentTransaction.commit()
+
+
     }
 
-
 }
+
+
